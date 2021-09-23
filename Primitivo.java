5736df22d73035ab3ddafc0f4c73547491f8898e
jaimepr01 <91058573+jaimepr01@users.noreply.github.com>
2021-09-23 10:16:08 +0200

    Create Primitivo.java

---
+++ Primitivo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primitivo;
+
+/**
+ *
+ * @author jaime
+ */
+public class Primitivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte varByte = 5;
+        System.out.println("Rango mínimo del Byte:" + Byte.MIN_VALUE);
+        System.out.println("Rango máximo del Byte:" + Byte.MAX_VALUE);
+    }
+    
+}
